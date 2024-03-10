7b49a5661c2b9c1e34fe984290e67b4869656012
Ryan Nolan <ryannolan_@hotmail.com>
2024-03-10 22:46:29 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -12,8 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('PP3-Python')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-data = sales.get_all_values()
+        data_str = input("Enter your data here:\n")
 
-print(data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
